84c827cd4c21f88734ff7a304b1109df0f053851
Frank Thomsen <frank@test.openesdh.dk>
2016-06-08 16:25:33 +0200

    change header layout

--- app/src/order_management/OrderDetailController.js
+++ app/src/order_management/OrderDetailController.js
@@ -7,39 +7,16 @@ function OrderDetailController($stateParams, ordermanagementService) {
     var odCtrl = this;
     odCtrl.orderId = $stateParams.orderid;
     odCtrl.data = [];
+    odCtrl.archivists = [
+        'none',
+        'Mogens',
+        'Peter',
+        'Jette',
+        'Xin'
+    ];
 
     ordermanagementService.getOrder(odCtrl.orderId).then(function(response) {
         odCtrl.data = response;
     });
-    
-    /*
-    odCtrl.data = {
-		"title": "Example order title",
-		"origin": null,
-		"endUserOrderNote": "Please provide this order as fast as possible",
-		"orderDate": "2016-05-18 12:00:00",
-		"plannedDate": "2016-05-18 12:00:00",
-		"user": {
-			"uid": "uid1",
-			"firstname": "Clint",
-			"lastname": "Eastwood",
-			"email": "clint@hollywood.com"		
-		},
-		"items": [
-            {
-                "title": "Item1",
-                "aipURI": "http://xyz.org/path",
-                "aipTitle": "This is the AIP title",
-                "levelOfDescription": 123
-            },
-            {
-                "title": "Item2",
-                "aipURI": "http://xyz.org/path2",
-                "aipTitle": "This is the AIP title 2",
-                "levelOfDescription": 1234
-            }
-		]
-    }
-    */
       
 };
