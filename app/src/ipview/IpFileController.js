1fa2393048a083c7ffbf6b41fc90d90260e7363c
Frank Thomsen <frank@test.openesdh.dk>
2016-11-02 10:25:52 +0100

    nicify file view

--- app/src/ipview/IpFileController.js
+++ app/src/ipview/IpFileController.js
@@ -7,17 +7,28 @@ function IpFileController($stateParams, ipViewService) {
     var ipfc = this;
     
     ipfc.data = {};
+    ipfc.info = {};
     ipfc.filePath = $stateParams.path;
-    //ipfc.parentPath = ipfc.filePath.substring(0, ipfc.filePath.lastIndexOf("/"));
     
     function getFileContent() {
-        var action = ipViewService.serializeObj({ action: 'getcontent', path: ipfc.filePath });
-        ipViewService.executeAction(action).then(
+        var contentAction = ipViewService.serializeObj({ action: 'getcontent', path: ipfc.filePath });
+        var infoAction = ipViewService.serializeObj({ action: 'getinfo', path: ipfc.filePath });
+        ipViewService.executeAction(contentAction).then(
             function (response) {
                 ipfc.data = response;
             },
             function (err) {
                 console.log('Error getting file content: ' + err.message);
+                errorService.displayErrorMsg( $translate.instant('IPVIEW.ERROR.MESSAGE.GET_CONTENT_ERROR') );
+            }
+        );
+        
+        ipViewService.executeAction(infoAction).then(
+            function (response) {
+                ipfc.info = response;
+            },
+            function (err) {
+                console.log('Error getting file info: ' + err.message);
                 errorService.displayErrorMsg( $translate.instant('IPVIEW.ERROR.MESSAGE.GET_CONTENT_ERROR') );
             }
         );
